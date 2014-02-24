5c7f3023b3f5e5bafb68cb2a68387cf1f6f91f2e
Anthony Chua <anthony@milkfishstudio.com>
2014-02-25 07:36:20 +0800

    Update HDAddress.php

--- HDAddress.php
+++ HDAddress.php
@@ -89,23 +89,22 @@ class gmp_Utils {public static function gmp_mod2($n,$d){if(extension_loaded('gmp
 
 
 class CryptUtil {
-
-	public static function devurandom_rand($min = 0, $max = 0x7FFFFFFF) {
-		$diff = $max - $min;
-		if ($diff < 0 || $diff > 0x7FFFFFFF) {
-		throw new RuntimeException("Bad range");
-		}
-		$bytes = mcrypt_create_iv(4, MCRYPT_DEV_URANDOM);
-		if ($bytes === false || strlen($bytes) != 4) {
-			throw new RuntimeException("Unable to get 4 bytes");
-		}
-		$ary = unpack("Nint", $bytes);
-		$val = $ary['int'] & 0x7FFFFFFF;   // 32-bit safe                           
-		$fp = (float) $val / 2147483647.0; // convert to [0,1]                          
-		return round($fp * $diff) + $min;
-	}
-
+    public static function devurandom_rand($min = 0, $max = 0x7FFFFFFF) {
+        $diff = $max - $min;
+        if ($diff < 0 || $diff > 0x7FFFFFFF) {
+            throw new RuntimeException("Bad range");
+        }
+        $bytes = mcrypt_create_iv(4, MCRYPT_DEV_URANDOM);
+        if ($bytes === false || strlen($bytes) != 4) {
+            throw new RuntimeException("Unable to get 4 bytes");
+        }       
+        $ary = unpack("Nint", $bytes);
+        $val = $ary['int'] & 0x7FFFFFFF;   // 32-bit safe                           
+        $fp = (float) $val / 2147483647.0; // convert to [0,1]                          
+    return round($fp * $diff) + $min;
+    }
 }
+
 
 class HDAddress {
 
